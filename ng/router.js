d0e1d68317234715c9e5f42393a2608a85e1274f
Garik Nurmurodoff <garikn95@gmail.com>
2018-03-26 11:53:49 +0500

    changed angular app name

--- ng/router.js
+++ ng/router.js
@@ -1,8 +1,8 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it dboardApp
+	var dboardApp = angular.module('dboard-app', ['ngRoute']);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	dboardApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
@@ -25,15 +25,15 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+	dboardApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
+	dboardApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an about page.';
 	});
 
-	scotchApp.controller('contactController', function($scope) {
+	dboardApp.controller('contactController', function($scope) {
 		$scope.message = 'Contact us! JK. This is just a demo.';
 	});
